7bd74536d8154ea48186acd423c97156f136f4b7
Samir Hafez <rhymecheat@hotmail.com>
2015-01-15 23:29:07 +0000

    IPAddress IPEndPoint

---
+++ MonoTorrent/IPAddress.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net
+{
+    public class IPAddress
+    {
+        public IPAddress(long address)
+        {
+
+        }
+
+        public byte[] GetAddressBytes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int HostToNetworkOrder(int order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NetworkToHostOrder(int order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public short HostToNetworkOrder(short order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long NetworkToHostOrder(long order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public short NetworkToHostOrder(short order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IPAddress Parse(string addres)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MonoTorrent/IPEndPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net
+{
+    public class IPEndPoint
+    {
+        public IPAddress Address { get; private set; }
+
+        public int Port { get; private set; }
+
+        public IPEndPoint(IPAddress address, int port)
+        {
+            Address = address;
+            Port = port;
+        }
+    }
+}
